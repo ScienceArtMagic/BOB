c616f55f511303379dd18cc5b37587ce0314b8eb
stereoplegic <me@mikebybee.com>
2024-12-12 17:14:01 -0600

    Create ste.py

---
+++ utils/ste.py
@@ -0,0 +1,19 @@
+import torch
+
+# https://hassanaskary.medium.com/intuitive-explanation-of-straight-through-estimators-with-pytorch-implementation-71d99d25d9d0
+class STEFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        return (input > 0).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return F.hardtanh(grad_output)
+
+class StraightThroughEstimator(nn.Module):
+    def __init__(self):
+        super(StraightThroughEstimator, self).__init__()
+
+    def forward(self, x):
+            x = STEFunction.apply(x)
+            return x
