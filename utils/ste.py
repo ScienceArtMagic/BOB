3bee7cfa63fcd99bd85f95693a4c19b469c77707
stereoplegic <me@mikebybee.com>
2025-03-02 03:06:16 -0600

    Update utils/ste.py

--- utils/ste.py
+++ utils/ste.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class STEFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, soft):
+    def forward(ctx, input: torch.Tensor, soft: bool = False) -> torch.Tensor:
         ctx.save_for_backward(input)
         ctx.soft = soft
         return (input > 0).float()
@@ -17,12 +17,12 @@ class STEFunction(torch.autograd.Function):
             grad_input = grad_output * F.softsign(input)  # Soft gradient
         else:
             grad_input = grad_output.clamp(min=-1, max=1)  # Hard gradient
-        return grad_input, None  # None for the `soft` argument
+        return grad_input
 
 
 class StraightThroughEstimator(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x, soft=False):
+    def forward(self, x: torch.Tensor, soft: bool = False) -> torch.Tensor:
         return STEFunction.apply(x, soft)
